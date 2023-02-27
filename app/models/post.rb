0ce0dccbf6408f446992d66543d7c5583645b3a0
NaveenVerma99 <naveen.verma@metafic.co>
2023-02-27 14:11:45 +0530

    delete-action-post_spec

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
+
+    validates :title, :presence => true, :length => { :minimum => 0, :allow_nil => false, :message => "can't be nil" }
 end
--- spec/controller/posts_controller_spec.rb
+++ spec/controller/posts_controller_spec.rb
@@ -24,24 +24,18 @@ RSpec.describe PostsController, :type => :controller do
 
     describe 'Post create' do 
         let(:faux_post) {FactoryBot.create :post}
-
+        
         it 'should accept the params with html format' do
             post :create, params: {
-                post: {
-                    title: "New Blog Post",
-                    body: "Body of new Blog Post"
-                }
+                post: post_params
             }
             expect(response.media_type).to eq('text/html')
             expect(response.content_type).to eq('text/html; charset=utf-8')
         end
-
+        
         it 'should accept the params with json format' do
             post :create, params: {
-                post: {
-                    title: "New Blog Post",
-                    body: "Body of new Blog Post"
-                },
+                post: post_params,
                 format: :json
             }
             expect(response.media_type).to eq('application/json')
@@ -50,23 +44,125 @@ RSpec.describe PostsController, :type => :controller do
 
         it 'should redirect to post showpage with html format' do
             post :create, params: {
-                post: {
-                    title: "New Blog Post",
-                    body: "Body of new Blog Post"
-                }
+                post: post_params
             }
             expect(subject).to redirect_to(assigns(:post))
         end
         it 'should render the show template with json format' do
             post :create, params: {
+                post: post_params,
+                format: :json
+            }
+            expect(subject).to render_template("posts/show") 
+        end
+
+        it 'should render the validation error' do
+            post :create, params: {
                 post: {
-                    title: "New Blog Post",
+                    title: nil,
                     body: "Body of new Blog Post"
                 },
                 format: :json
             }
-            expect(subject).to render_template("posts/show")
+            expect(assigns(:post).valid?).to_not eq(true)
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'should should include the error messages for empty attributes' do
+            post :create, params: {
+                post: {
+                    title: nil,
+                    body: "Body of new Blog Post"
+                },
+                format: :json
+            }
+            expect(assigns(:post).errors.full_messages).to include("Title can't be blank")
         end
     end
 
+    describe 'Patch Update' do 
+        let(:faux_post) {FactoryBot.create :post}
+        let(:faux_post2) {FactoryBot.create :post}
+        let(:faux_post3) {FactoryBot.create :post}
+
+        it 'should accept the params with html format' do
+            patch :update, params: {
+                post: post_params,
+                id: faux_post2.id
+            }
+            expect(response.media_type).to eq('text/html')
+            expect(response.content_type).to eq('text/html; charset=utf-8')
+        end
+
+        it 'should accept the params with json format' do
+            patch :update, params: {
+                post: post_params,
+                id: faux_post2.id,
+                format: :json
+            }
+            expect(response.media_type).to eq('application/json')
+            expect(response.content_type).to eq('application/json; charset=utf-8')
+        end
+
+        it 'should redirect to post showpage with html format' do
+            patch :update, params: {
+                post: post_params,
+                id: faux_post2.id,
+            }
+            expect(subject).to redirect_to(assigns(:post))
+        end
+        it 'should render the show template with json format' do
+            patch :update, params: {
+                post: post_params,
+                id: faux_post2.id,
+                format: :json
+            }
+            expect(subject).to render_template("posts/show") 
+        end
+
+        it 'should render the validation error' do
+            patch :update, params: {
+                post: {
+                    title: nil,
+                    body: "Body of new Blog Post"
+                },
+                id: faux_post2.id,
+                format: :json
+            }
+            expect(assigns(:post).valid?).to_not eq(true)
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'should should include the error messages for empty attributes' do
+            patch :update, params: {
+                post: {
+                    title: nil,
+                    body: "Body of new Blog Post"
+                },
+                id: faux_post2.id,
+                format: :json
+            }
+            expect(assigns(:post).errors.full_messages).to include("Title can't be blank")
+        end
+    end
+
+    describe 'DELETE Destroy' do
+        let!(:faux_post) {FactoryBot.create(:post)}
+        let!(:faux_post1) {FactoryBot.create :post}
+    
+        it 'should reduce the user count by one' do
+            expect do
+                delete :destroy, params: { id: faux_post.id, format: :json }
+            end.to change(Post, :count).by(-1)
+        
+            expect(response).to be_successful
+        end
+    end
+
+    def post_params
+        {
+            title: "New Blog Post",
+            body: "Body of new Blog Post"
+        }
+    end
 end
